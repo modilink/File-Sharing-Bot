cf942bc7b0cf3808f8f83ca97e117f530d87df97
vishhalllll <108014711+vishhalllll@users.noreply.github.com>
2023-12-29 18:22:58 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>@like5kai</a>\n○ Channel : @ViralLinkWallah12",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>@illegalOnwerBot</a>\n○ Channel : @ViralLinkWallah12",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
